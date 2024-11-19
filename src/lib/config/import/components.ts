47f9b323f76a1348eeb4fbcd640e7dbae01246fd
Shimmy <shimmyrukundo@gmail.com>
2024-11-19 18:44:06 -0500

    will finish and publish utica today

--- src/lib/config/import/components.ts
+++ src/lib/config/import/components.ts
@@ -1,15 +1,9 @@
 
 /** application */
-export { default as UCombobox } from '../../components/application/combobox/combobox.svelte';
-export { default as UCommandMenu } from '../../components/application/command/_menu.svelte';
 export { default as UDashLayout } from '../../components/application/dash/layout.svelte';
 export { default as UField } from '../../components/application/field/field.svelte';
 export { default as UFormDebug } from '../../components/application/form/debug.svelte';
 export { default as UForm } from '../../components/application/form/form.svelte';
-export { default as UHeadbar } from '../../components/application/headbar/headbar.svelte';
-export { default as UMenuItem } from '../../components/application/menu/item.svelte';
-export { default as UMenu } from '../../components/application/menu/menu.svelte';
-export { default as UMenubar } from '../../components/application/menubar/menubar.svelte';
 export { default as UModal } from '../../components/application/modal/modal.svelte';
 export { default as UPaginate } from '../../components/application/paginate/paginate.svelte';
 export { default as URadio } from '../../components/application/radio/radio.svelte';
@@ -24,13 +18,10 @@ export { default as UAccordion } from '../../components/common/accordion/accordi
 export { default as UAccordionItem } from '../../components/common/accordion/item.svelte';
 export { default as UAvatar } from '../../components/common/avatar/avatar.svelte';
 export { default as UAvatarGroup } from '../../components/common/avatar/group.svelte';
-export { default as UBadge } from '../../components/common/badge/badge.svelte';
 export { default as UButton } from '../../components/common/button/button.svelte';
-export { default as UCalendar } from '../../components/common/calendar/calendar.svelte';
 export { default as UCard } from '../../components/common/card/card.svelte';
 export { default as UCheckbox } from '../../components/common/checkbox/checkbox.svelte';
 export { default as UContainer } from '../../components/common/container/container.svelte';
-export { default as UDropdown } from '../../components/common/dropdown/dropdown.svelte';
 export { default as UInputDate } from '../../components/common/input/date.svelte';
 export { default as UInputGroup } from '../../components/common/input/group.svelte';
 export { default as UInput } from '../../components/common/input/input.svelte';
@@ -38,8 +29,6 @@ export { default as UInputPin } from '../../components/common/input/pin.svelte';
 export { default as UInputTag } from '../../components/common/input/tag.svelte';
 export { default as UProgress } from '../../components/common/progress/progress.svelte';
 export { default as UProvider } from '../../components/common/provider/provider.svelte';
-export { default as URange } from '../../components/common/range/range.svelte';
-export { default as USelectMenu } from '../../components/common/select/menu.svelte';
 export { default as USelect } from '../../components/common/select/select.svelte';
 export { default as USwitch } from '../../components/common/switch/switch.svelte';
 export { default as UToast } from '../../components/common/toast/toast.svelte';
@@ -47,16 +36,13 @@ export { default as UToggle } from '../../components/common/toggle/toggle.svelte
 export { default as UTooltip } from '../../components/common/tooltip/tooltip.svelte';
 
 /** website */
-export { default as UAside } from '../../components/website/aside/aside.svelte';
-export { default as UChip } from '../../components/website/chip/chip.svelte';
 export { default as UCollapsible } from '../../components/website/collapsible/collapsible.svelte';
-export { default as UContextMenu } from '../../components/website/context/_menu.svelte';
+export { default as UDescription } from '../../components/website/description/description.svelte';
 export { default as UFooter } from '../../components/website/footer/footer.svelte';
 export { default as UFooterItem } from '../../components/website/footer/item.svelte';
 export { default as UHeaderAside } from '../../components/website/header/aside.svelte';
 export { default as UHeader } from '../../components/website/header/header.svelte';
 export { default as UHeaderItem } from '../../components/website/header/item.svelte';
-export { default as UHeading } from '../../components/website/title/title.svelte';
 export { default as UIcon } from '../../components/website/icon/icon.svelte';
 export { default as ULabel } from '../../components/website/label/label.svelte';
 export { default as UPopover } from '../../components/website/popover/popover.svelte';
@@ -64,3 +50,4 @@ export { default as UPriceCard } from '../../components/website/price/card.svelt
 export { default as UPrice } from '../../components/website/price/price.svelte';
 export { default as UScroll } from '../../components/website/scroll/scroll.svelte';
 export { default as UTab } from '../../components/website/tab/tab.svelte';
+export { default as UTitle } from '../../components/website/title/title.svelte';
